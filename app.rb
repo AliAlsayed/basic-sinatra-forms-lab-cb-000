7d068ed71dc34591a20f32bf6fd9b9c7cbfef737
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-16 07:48:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
              'shooting_guard'=>params[:sg],
              'small_forward'=>params[:sf],
              'power_forward'=>params[:pf],
-             'center'=>params[:center]}
+             'center'=>params[:c]}
     erb :team
   end
 end
